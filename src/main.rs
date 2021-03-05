f412fc2a93074746923c64ff90eba8d4cf091e5b
Ha ĐANG <dvietha@gmail.com>
2021-03-05 20:19:42 +0700

    add sample for advanced trait

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::fmt;
+use std::ops::Add;
+use std::ops::Shl;
+
 use ethereum_types::U512;
-use std::ops::{Shl};
 
 pub trait Draw {
     fn draw(&self);
@@ -40,7 +43,74 @@ fn set_compact(compact: u32) -> U512 {
     }
 }
 
+// Implementing the Add trait to overload the + operator for Point instances
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+impl OutlinePrint for Point {}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+// Implementing the Add trait on Millimeters to add Millimeters to Meters
+struct Millimeters(u32);
+
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+// Creating a Wrapper type around Vec<String> to implement Display
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
 fn main() {
     let work = set_compact(0x18abcdef);
     println!("{}", work);
+    println!("p = {}", Point { x: 1, y: 0 } + Point { x: 2, y: 3 });
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
