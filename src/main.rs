138c934e9be4d1efdd879671b93be2720c0245aa
Ha ĐANG <dvietha@gmail.com>
2021-03-06 10:27:36 +0700

    understanding the lifetime of parameter

--- src/main.rs
+++ src/main.rs
@@ -136,7 +136,21 @@ macro_rules! to_string_vec {
     };
 }
 
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
     let strs = to_string_vec![1, 2, 3];
     for str in strs.iter() {
         println!("{}", str)
